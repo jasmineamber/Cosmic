66c270587a7f300965e5280e990b1d7a0cb0bda4
FongMi <b8892778@gmail.com>
2025-04-19 17:08:22 +0800

    Add expbuff

--- src/main/java/client/command/commands/gm0/RatesCommand.java
+++ src/main/java/client/command/commands/gm0/RatesCommand.java
@@ -23,6 +23,7 @@
 */
 package client.command.commands.gm0;
 
+import client.BuffStat;
 import client.Character;
 import client.Client;
 import client.command.Command;
@@ -36,10 +37,12 @@ public class RatesCommand extends Command {
     @Override
     public void execute(Client c, String[] params) {
         Character player = c.getPlayer();
+        Integer expBuff = player.getBuffedValue(BuffStat.EXP_BUFF);
+        expBuff = expBuff != null ? expBuff / 100 : 1;
 
         // travel rates not applicable since it's intrinsically a server/environment rate rather than a character rate
         String showMsg_ = "#eCHARACTER RATES#n" + "\r\n\r\n";
-        showMsg_ += "EXP Rate: #e#b" + player.getExpRate() + "x#k#n" + (player.hasNoviceExpRate() ? " - novice rate" : "") + "\r\n";
+        showMsg_ += "EXP Rate: #e#b" + player.getExpRate() * expBuff + "x#k#n" + (player.hasNoviceExpRate() ? " - novice rate" : "") + "\r\n";
         showMsg_ += "MESO Rate: #e#b" + player.getMesoRate() + "x#k#n" + "\r\n";
         showMsg_ += "DROP Rate: #e#b" + player.getDropRate() + "x#k#n" + "\r\n";
         showMsg_ += "BOSS DROP Rate: #e#b" + player.getBossDropRate() + "x#k#n" + "\r\n";
